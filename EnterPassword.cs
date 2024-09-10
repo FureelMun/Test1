bb5f61a056b01f9571697a3c60f9a6b98c059c7f
Elizaveta <84278722+FureelMun@users.noreply.github.com>
2024-09-11 00:08:27 +0300

    Add files via upload

---
+++ EnterPassword.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using System.Linq;
+
+using System.Text.RegularExpressions;
+
+
+namespace FirstTest
+
+{
+
+    internal class EnterPassword
+
+    {
+        // Метод для проверки длины пароля
+        public int Enter_contain10(string password)
+        {
+            return password.Length;
+        }
+        // Метод для подсчета специальных символов в пароле
+        public int Enter_containSpecS(string password)
+        {
+            var regex = new Regex(@"[\W_]");
+            return password.Count(c => regex.IsMatch(c.ToString()));
+        }
+        public int Enter_containNum(string password)
+        {
+            var regex = new Regex(@"[0-9]");
+            return password.Count(c => regex.IsMatch(c.ToString()));
+        }
+        public int Enter_containZog(string password)
+        {
+            var regex = new Regex(@"[A-Z]");
+            return password.Count(c => regex.IsMatch(c.ToString()));
+        }
+        public int Enter_containNZog(string password)
+        {
+            var regex = new Regex(@"[a-z]");
+            return password.Count(c => regex.IsMatch(c.ToString()));
+        }
+    }
+
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace FirstTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Password_10contain10_return()
+        {
+            string password = "Test123@!'";
+            int right_password = 10;
+
+            EnterPassword enterPassword = new EnterPassword();
+            int actual = enterPassword.Enter_contain10(password);
+            Assert.AreEqual(actual, right_password);
+
+        }
+        [TestMethod]
+        public void Password_PasContainSp_return()
+        {
+            string password = "Test123@!'";
+            var regex = new Regex(@"[\W_]");
+            int contain = password.Count(c => regex.IsMatch(c.ToString()));
+
+            EnterPassword enterPassword = new EnterPassword();
+            int actual = enterPassword.Enter_containSpecS(password);
+            Assert.AreEqual(actual, contain);
+
+        }
+        [TestMethod]
+        public void Password_PasContainNam_return()
+        {
+            string password = "Test123@!'";
+            var regex = new Regex(@"[0-9]");
+            int contain = password.Count(c => regex.IsMatch(c.ToString()));
+
+            EnterPassword enterPassword = new EnterPassword();
+            int actual = enterPassword.Enter_containNum(password);
+            Assert.AreEqual(actual, contain);
+
+        }
+        [TestMethod]
+        public void Password_PasContainZog_return()
+        {
+            string password = "Test123@!'";
+            var regex = new Regex(@"[A-Z]");
+            int contain = password.Count(c => regex.IsMatch(c.ToString()));
+
+            EnterPassword enterPassword = new EnterPassword();
+            int actual = enterPassword.Enter_containZog(password);
+            Assert.AreEqual(actual, contain);
+
+        }
+        [TestMethod]
+        public void Password_PasContainNZog_return()
+        {
+            string password = "Test123@!'";
+            var regex = new Regex(@"[a-z]");
+            int contain = password.Count(c => regex.IsMatch(c.ToString()));
+
+            EnterPassword enterPassword = new EnterPassword();
+            int actual = enterPassword.Enter_containNZog(password);
+            Assert.AreEqual(actual, contain);
+
+        }
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
